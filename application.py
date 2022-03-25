c22d4e63d5c5e0a0625670d0e46c82fc7b10d60e
Cinthia <cinthiaduarte013@gmail.com>
2022-03-25 15:48:52 -0600

    registro hecho

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+
 import os
 
 from flask import Flask, session, flash, redirect, render_template, request, session, url_for, jsonify
@@ -5,12 +6,13 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from dotenv import load_dotenv
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
 # Check for environment variable
-##if not os.getenv("DATABASE_URL"):
-  #  raise RuntimeError("DATABASE_URL is not set")
+# if not os.getenv("DATABASE_URL"):
+#  raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -26,8 +28,30 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     return render_template("inicio.html")
 
-@app.route("/register")
-def register():
-  return render_template("register.html")
 
-@app.route()
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        user = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        if confirmation != password:
+            flash("passwords dont match")
+            return render_template("register.html")
+
+        rows = db.execute(
+            "SELECT usuario FROM users WHERE usuario=:username", {"username": user}).fetchall()
+
+        if not rows:
+            db.execute("INSERT INTO users (usuario, contraseña) VALUES (:username, :password)",
+                       {"username": user, "password": generate_password_hash(password)})
+            db.commit()
+        elif rows:
+            flash("username already exists")
+            return render_template("register.html")
+
+        return redirect("/")
+
+    else:
+        return render_template("register.html")
